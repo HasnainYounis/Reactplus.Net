1ec442cfe260f2a34a615ddfd72a588cc5dfadf7
Ali Hasnain <hasnain.younis765@gmail.com>
2024-05-03 15:44:51 +0500

    lec55 usecontext

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import UseEffectExamples from './UseEffectExample';
 import './App.css';
+import Grandparent from './Grandparent';
+import ValueContext from './ValueContext';
 
 function App() {
   // it hide the and show the text ontick on checkbox
@@ -10,7 +12,10 @@ function App() {
    <>
    <input type="checkbox" onChange={()=>setHide(!hide)} />  
    {hide ? null: <UseEffectExamples />}
-    
+   
+   <ValueContext.Provider value={hide}>
+      <Grandparent />
+      </ValueContext.Provider>
     </>
   )
 }
--- src/SelectNumber.tsx
+++ src/SelectNumber.tsx
@@ -1,12 +1,9 @@
 export default function SelectNumber(props: selectNumberProps){
     const arr = Array(props.maxValue).fill(0);
     return(
-        
-        <select onChange ={(e) =>{
+         <select onChange ={(e) =>{
             console.log(e.currentTarget.value);
-            props.onSelected(parseInt(e.currentTarget.value,10));
-          //  setSelectedRate(parseInt(e.currentTarget.value, 10));
-               
+            props.onSelected(parseInt(e.currentTarget.value,10));        
        }}>
          {arr.map((_, index)=><option key={index+1} value={index+1}>{props.selectContent(index+1)}</option>)}
             {/* <option value={1}>1</option>
@@ -14,10 +11,8 @@ export default function SelectNumber(props: selectNumberProps){
             <option value={3}>3</option>
             <option value={4}>4</option>
             <option value={5}>5</option> */}
-
-        </select>
-      
-    )
+         </select>
+     )
 }
 interface selectNumberProps{
     maxValue:number;
