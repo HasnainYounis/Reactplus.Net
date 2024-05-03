e3069f8aeeca62ff3a7574ee0ec961107e1a8b0c
Ali Hasnain <hasnain.younis765@gmail.com>
2024-05-03 11:19:27 +0500

    lec 54 useEffect title work

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import UseEffectExamples from './UseEffectExample';
 import './App.css';
 
 function App() {
+  // it hide the and show the text ontick on checkbox
   const [hide, setHide]= useState(false);
   return (
    <>
--- src/UseEffectExample.tsx
+++ src/UseEffectExample.tsx
@@ -1,14 +1,27 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export default function UseEffectExamples(){
+    const [clicks, setClicks] = useState(0);
+
     useEffect(()=>{
         //Executed immediately
         console.log('component loaded');
-    })
+        return () => {
+            {/* run before the component is destroyed */}
+           console.log ('The component will be destoryed');
+        }
+    },[]);
+    useEffect(() =>{
+        document.title = `${clicks} times`;
+
+    },[clicks])
     return(
         <>
-        {/* run before the component is destroyed */}
-           console.log ('The component will be destoryed');
+        <h1>UseEffect Examples</h1>
+        <div>
+            <button onClick={()=> setClicks(clicks+1)}>
+                you have clicked {clicks} times </button>
+        </div>
         </>
     )
 }
